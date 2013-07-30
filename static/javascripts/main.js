191ea2b6605b7c5238b1578c4abcc963cf31ce91
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2013-07-30 18:30:53 +0100

    Made menu responsive.

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -1,24 +1,36 @@
 // Gumby is ready to go
-Gumby.ready(function() {
-	console.log('Gumby is ready to go...', Gumby.debug());
+Gumby.ready(function () {
+    console.log('Gumby is ready to go...', Gumby.debug());
 
-	// placeholder polyfil
-	if(Gumby.isOldie || Gumby.$dom.find('html').hasClass('ie9')) {
-		$('input, textarea').placeholder();
-	}
+    // placeholder polyfil
+    if (Gumby.isOldie || Gumby.$dom.find('html').hasClass('ie9')) {
+        $('input, textarea').placeholder();
+    }
 });
 
 // Oldie document loaded
-Gumby.oldie(function() {
-	console.log("This is an oldie browser...");
+Gumby.oldie(function () {
+    console.log("This is an oldie browser...");
 });
 
-Gumby.touch(function() {
-	console.log("This is a touch enabled device...");
+Gumby.touch(function () {
+    console.log("This is a touch enabled device...");
 });
 
 // Document ready
-$(function() {
+$(function () {
+    $('.control a').click(function (event) {
+        event.preventDefault();
 
+        var menu = $('#menu_items');
+
+        if (menu.is(':visible')) {
+            menu.slideUp();
+            $(this).html($(this).html().replace('Hide', 'Show'));
+        } else {
+            menu.slideDown();
+            $(this).html($(this).html().replace('Show', 'Hide'));
+        }
+    });
 });
 
