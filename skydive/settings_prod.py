82bd850a2ed98869628d75ecfca4f8b6d33a7955
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2013-07-30 18:36:28 +0100

    Updated production settings.

--- skydive/settings_prod.py
+++ skydive/settings_prod.py
@@ -5,7 +5,7 @@ Production server settings.
 from settings_base import *
 from os import getenv
 
-ROOT = ''
+ROOT = '/home/skydive/site/'
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -26,6 +26,7 @@ DATABASES = {
 ALLOWED_HOSTS = [
     'localhost',
     'skydive.dev',
+    'lsd.jsherz.com',
 ]
 
 # Sites module ID
@@ -38,7 +39,7 @@ MEDIA_ROOT = ROOT + 'media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://www.skydive.dev/media/'
+MEDIA_URL = 'http://lsd.jsherz.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
