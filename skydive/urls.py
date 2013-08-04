b36eee2687a63c4e0ca7d05c82b4d8bb4ff7d2cf
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2013-08-04 20:21:45 +0100

    Added dummy views and linked up menu.

--- skydive/urls.py
+++ skydive/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'skydive.views.home', name='home'),
+                       # App views
+                       url(r'^$', 'skydive.views.home', name='home'),
+                       url(r'^why-skydive$', 'skydive.views.why_skydive', name='why_skydive'),
+                       url(r'^learn-to-skydive$', 'skydive.views.learn_to_skydive', name='learn_to_skydive'),
+                       url(r'^contact-us$', 'skydive.views.contact_us', name='contact_us'),
 
-    # url(r'^ukbiz/', include('skydive.foo.urls')),
-
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+                       # Admin
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
 )
--- skydive/views.py
+++ skydive/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def why_skydive(request):
+    return render(request, 'index.html')
+
+
+def learn_to_skydive(request):
+    return render(request, 'index.html')
+
+
+def contact_us(request):
+    return render(request, 'index.html')
