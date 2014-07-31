e7a0873f48ec839feedcb9054daca617ba863305
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-07-31 16:15:01 +0100

    Added helper to allow current page to be highlighted on navbar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def navbar_link(url, text)
+    # TODO: Made less awful
+    raw("<li>
+          <i class=\"seperator fa fa-ellipsis-v\"></i>
+        </li>
+        <li>
+          <a href=\"#{url}\"#{' class="active"' if current_page? url}>
+            #{text}
+          </a>
+        </li>")
+  end
 end
