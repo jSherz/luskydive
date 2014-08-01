c5cd534d45b9ffcf6db59deaec5defd207d08f28
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-08-01 08:56:18 +0100

    Added basic unicorn config file.

---
+++ config/unicorn.rb
@@ -0,0 +1,15 @@
+base_path = '/home/site/webapps/luskydive/'
+log_path = base_path + 'log/unicorn.log'
+
+working_directory base_path
+
+pid base_path + 'pids/unicorn.pid'
+
+stderr_path log_path
+stdout_path log_path
+
+listen '/tmp/unicorn.luskydive.sock'
+
+worker_processes 4
+
+timeout 30
