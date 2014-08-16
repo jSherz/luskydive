7b430a9e06c6d45bb4cc72ff21c4d71834ecf68b
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-08-16 11:06:13 +0100

    Added validation & weighting help text to committee member. Hid versions field.

--- app/models/committee_member.rb
+++ app/models/committee_member.rb
@@ -1,5 +1,23 @@
 class CommitteeMember < ActiveRecord::Base
-  # TODO: Add validation
+  validates :name, presence: true
+  validates :role, presence: true
+  validates :weighting, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
+  validates :image, presence: true
+
   # TOOD: Allow uploads of new committee photos
+
   has_paper_trail
+
+  rails_admin do
+    edit do
+      field :name
+      field :role
+
+      field :weighting do
+        help 'Required. People with higher weightings apear first on the page.'
+      end
+      
+      field :image
+    end
+  end
 end
