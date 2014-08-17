76bf7c1f041342ca88090823b1c65836c45b2692
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-08-17 16:21:42 +0100

    Cleaned up FAQs.

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -5,5 +5,6 @@ class FaqsController < ApplicationController
     id = params[:id] || @faq_categories.first.id
 
     @faq_category = @faq_categories.find id
+    @faqs = @faq_category.faqs
   end
 end
