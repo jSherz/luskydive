23ddfd5bf067c5b0fd6a4daa6a82e9def775f4ec
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-08-17 16:17:26 +0100

    Cleaned up navbar items.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,12 @@
 module ApplicationHelper
   def navbar_link(url, text, li_id = nil)
-    # Make url into an array if not already
-    urls = [*url]
+    @urls = Array url
 
-    active_link = !urls.select { |url,_| current_page? url }.empty?
+    @active_link = @urls.select { |url,_| current_page? url }.any?
 
-    raw("<li>
-          <i class=\"seperator fa fa-ellipsis-v\"></i>
-        </li>
-        <li id=\"#{li_id || ''}\">
-          <a href=\"#{urls.first}\"#{' class="active"' if active_link}>
-            #{text}
-          </a>
-        </li>")
+    render partial: 'navbar_item', locals: {
+      li_id: li_id,
+      text: text
+    }
   end
 end
