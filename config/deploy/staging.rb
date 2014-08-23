9e92ce84df96bbc3a157342af9b5c7c804eba1b0
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-08-23 10:38:11 +0100

    Added staging environment (pi).

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{jsj-pi}
+role :web, %w{jsj-pi}
+role :db,  %w{jsj-pi}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'jsj-pi', user: 'site', roles: %w{web app db}
 
 
 # Custom SSH Options
@@ -25,11 +25,10 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, {
+  keys: %w(/home/james/.ssh/jsj_linux),
+  auth_methods: %w(publickey)
+}
 #
 # And/or per server (overrides global)
 # ------------------------------------
