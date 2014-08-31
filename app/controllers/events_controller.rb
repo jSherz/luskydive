2a3256d8c46d77c60420be44ff4e1375d8a4cbe0
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-08-31 11:11:51 +0100

    Added rescue condition & error page if event lookup fails.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,12 @@
 # Displays events, pulled from Facebook
 class EventsController < ApplicationController
+  rescue_from Koala::Facebook::AuthenticationError, with: :lookup_error
+
   def index
     @events = Event.all
+  end
+
+  def lookup_error
+    render 'lookup_error', status: 500
   end
 end
